55f0e9e8303882be09556bdbcbc04d006baf697d
weng lin <wenglin@buffalo.edu>
2020-01-03 19:29:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ require 'yaml'
 def load_library(file_path)
   library = YAML.load_file(file_path)
   result = {"get_meaning" => {}, "get_emoticon" => {}}
-  library.each_pair {|meaning, emoticons|
+  library.each_pair {|meaning, emoticon|
     result["get_meaning"][emoticon[1]] = meaning
     result["get_emoticon"][emoticon[0]] = emoticon[1]}
   end
