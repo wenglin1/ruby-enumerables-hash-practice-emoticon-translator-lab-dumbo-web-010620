c768d6566e7adae1578d40e8f6cf0c4f1f685fba
weng lin <wenglin@buffalo.edu>
2020-01-03 19:33:17 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,34 @@
-# require modules here
+require 'pry'
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  lib = YAML.load_file(file_path)
+  emoticon_data = { "get_meaning" => {}, "get_emoticon" => {} }
+
+  lib.each_pair { |emotion, face_array|
+    emoticon_data["get_meaning"][face_array.last] = emotion
+    emoticon_data["get_emoticon"][face_array.first] = face_array.last
+  }
+
+  emoticon_data
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path,emoticons)
+  lib = load_library(file_path)
+  lib["get_emoticon"].each_pair {|emoticon, meaning|
+    if emoticon == emoticons
+      return meaning
+    end
+  }
+  "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path,emoticons)
+  lib = load_library(file_path)
+  lib["get_meaning"].each_pair {|emoticon, meaning|
+    if emoticon == emoticons
+      return meaning
+    end
+  }
+  "Sorry, that emoticon was not found"
 end
